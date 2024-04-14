8cc92f373bed746e816fcf2e2db2d56235ddc4af
Gopiten <166960193+Gopiten@users.noreply.github.com>
2024-04-13 21:23:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <title>CompumundoHiperMegaRed</title>
+</head>
+<body>
+    <style>
+       body{
+        background-image: url('bg.jpg');
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        color: rgb(255, 255, 255);
+        font-weight: bold;
+        text-shadow: 5px 5px 5px #ffb3b3;
+        text-align: center;
+        align-items: center;
+       }
+       h1{
+        padding-top: 2%;
+        font-size: 75px;
+       }
+       div{
+        padding-left: 10%;
+        text-align: left;
+        font-size: 30px;
+       }
+       a{
+        color: rgb(255, 255, 255);
+       }
+    </style>
+    <h1>AGUSTIN GABRIEL ALVAREZ</h1>
+    <br>
+    <div>
+        <h3>Archivos</h3>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";>
+        <?php
+            echo "Tarea: holaMundo";
+        ?>
+        </a>
+    </div>
+    
+</body>
+</html>
